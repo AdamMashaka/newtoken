1eb1efc7b1c5578fcf6d0b8de089e57ca66492c6
Shubham0850 <rajshubham0850@gmail.com>
2024-04-02 18:26:25 +0530

    font updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Roboto } from 'next/font/google'
-
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-})
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={roboto.className}>
+      <body>
         <Main />
         <NextScript />
       </body>
